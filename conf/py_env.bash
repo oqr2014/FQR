ddc307d880908ca1c019f8985435ba579e1b489d
oqr2014 <oqr2014@gmail.com>
2014-06-06 21:55:19 -0500

    change dir name

--- conf/py_env.bash
+++ conf/py_env.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export LD_LIBRARY_PATH=/usr/lib:/usr/local/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=/oqr/Svr/Quote/py:/oqr/Libs/pricing/src/python:/oqr/MD/FIX/python:$LD_LIBRARY_PATH:$PYTHONPATH
+export PYTHONPATH=/oqr/Svr/Quote/py:/oqr/Libs/pricing/src/py:/oqr/MD/FIX/py:$LD_LIBRARY_PATH:$PYTHONPATH
 export PYTHONPATH=/oqr/Libs/utils/src/py:$PYTHONPATH
-export XML_CONF_DIR=/oqr/config/ 
+export XML_CONF_DIR=/oqr/conf/ 
